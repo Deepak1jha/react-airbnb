661d9a751a48fc5872d27e212b1da4842fe4d950
Deepak jha <ddjha171@gmail.com>
2020-08-30 21:13:04 +0530

    Search, Search Result added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/"
+                 render={props => <SearchPage {...props} />}/>
+          <Route path="/search"
+                 render={props => <SearchPage {...props} />}/>
+        </Switch>
+        <Footer/>
+      </Router>
     </div>
   );
 }
