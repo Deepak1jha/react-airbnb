7b36b78b6267f1dc185dd13798b490524738e66c
Deepak1jha <ddjha171@gmail.com>
2020-09-20 19:02:59 +0530

    Updates

---
+++ precache-manifest.16bdd06128d27e0e7bb295f45a5c9e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759226e412fa21532340e6fcebbe96d0",
+    "url": "/react-airbnb/index.html"
+  },
+  {
+    "revision": "9b5efe712235485d6caa",
+    "url": "/react-airbnb/static/css/2.9e0dcc19.chunk.css"
+  },
+  {
+    "revision": "a990ebfa46e31f8a18a1",
+    "url": "/react-airbnb/static/css/main.8137cfb1.chunk.css"
+  },
+  {
+    "revision": "9b5efe712235485d6caa",
+    "url": "/react-airbnb/static/js/2.c03bb3a1.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-airbnb/static/js/2.c03bb3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a990ebfa46e31f8a18a1",
+    "url": "/react-airbnb/static/js/main.b9e6dd49.chunk.js"
+  },
+  {
+    "revision": "b1b7ccadf3ac515cb096",
+    "url": "/react-airbnb/static/js/runtime-main.9a2bffe8.js"
+  }
+]);
